218c0123a532df26fea453d82387fb5ca2432222
noman shaber <nimrashaheen451@gmail.com>
2025-08-10 00:09:37 +0500

    Project Desgin Complete

--- store/forms.py
+++ store/forms.py
@@ -10,10 +10,19 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'block w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300'}),
+            'email': forms.EmailInput(attrs={'class': 'block w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300'}),
+            'password1': forms.PasswordInput(attrs={'class': 'block w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300'}),
+            'password2': forms.PasswordInput(attrs={'class': 'block w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300'}),
+        }
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+        widgets = {
+            field.name: forms.TextInput(attrs={'class': 'block w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300'})
+            for field in Product._meta.fields
+        }
